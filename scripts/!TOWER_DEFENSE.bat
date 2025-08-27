0facf90ff400443cc0d79528b9cf4d7f809881c5
Z-Tavern <eizekielsss@gmail.com>
2025-08-27 16:02:05 +0000

    setting up 1/2

---
+++ scripts/!TOWER_DEFENSE.bat
@@ -0,0 +1,25 @@
+@echo off
+::Paste the server key from https://platform.plutonium.pw/serverkeys here
+set key=FYWobSe3aZpkx7bHuFJGP4CbFplUNgVM
+::Name of the config file the server should use. (default: dedicated_zm.cfg)
+set cfg=dedicated_zmtower.cfg
+::Name of the server shown in the title of the cmd window. This will NOT bet shown ingame.
+set name=Tower
+::Port used by the server (default: 4976)
+set port=30000
+::Mod name (default "")
+set mod="mods\ztavern_pack"
+::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
+set gamepath=%cd%
+
+title Tower
+echo Visit plutonium.pw / Join the Discord (a6JM2Tv) for NEWS and Updates!
+echo Server "%name%" will load %cfg% and listen on port %port% UDP!
+echo To shut down the server close this window first!
+echo (%date%)  -  (%time%) %name% server start.
+
+cd /D %LOCALAPPDATA%\Plutonium
+:server
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe t6zm "%gamepath%" -dedicated +set key %key% +set fs_game %mod% +set net_port %port% +exec %cfg% +map_rotate
+echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
+goto server
