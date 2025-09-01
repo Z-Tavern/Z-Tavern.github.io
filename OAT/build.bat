11c7990aed593a728e28936066ce7d92889b9bee
Kiels <eizekielsss@gmail.com>
2025-09-01 09:51:44 +0200

    build bat updated

--- OAT/build.bat
+++ OAT/build.bat
@@ -1,2 +1,9 @@
-Unlinker.exe --include-assets soundalias "C:\Users\Kiels Main\Downloads\OATbo2\zm_prison.ff" -o C:\OAT_Dumps
+set GAME_FOLDER=C:\Users\Kiels Main\Downloads\pluto_t6_full_game
+set OAT_BASE=C:\Users\Kiels Main\Downloads\OATbo2
+set MOD_BASE=%cd%
+
+"%OAT_BASE%\Linker.exe" --verbose --load "%GAME_FOLDER%\zone\all\zm_transit.ff" --add-asset-search-path "%MOD_BASE%" mod
+
+powershell -Command "Compress-Archive -Force -Path images -DestinationPath mod.iwd"
+
 pause
